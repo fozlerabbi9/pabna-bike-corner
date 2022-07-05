970c0c204a688b9fd87ec0f073d02cf91a2c54e7
Fozle Rabbi <fozlerabbi9790@gmail.com>
2022-07-05 16:32:58 +0600

    cool

--- src/ComponentFile/HomeFile/BikerClubFile/BikerClubPage.js
+++ src/ComponentFile/HomeFile/BikerClubFile/BikerClubPage.js
@@ -7,8 +7,8 @@ const BikerClubPage = () => {
             <div className="title-div">
                 <p>We Are The <span>Biker</span>Club </p>
                 <div className="border-span">
-                    <span className='yellow'></span>
-                    <span className='red'></span>
+                    <span className='yellow-span'></span>
+                    <span className='red-span'></span>
                 </div>
                 <p className="card-text">A motorcycle, often called a motorbike, bike, cycle, or trike, is a two- or three-wheeled motor vehicle. Motorcycle design varies greatly to suit a range of different purposes: long-distance travel, commuting, cruising, sport, and off-road riding</p>
             </div>
@@ -25,8 +25,8 @@ const BikerClubPage = () => {
                     <div className="col">
                         <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 className="card-title">TopClass Performence</h5>
+                                    <p className="card-text">The 1885 Daimler Reitwagen made by Gottlieb Daimler and Wilhelm Maybach in Germany was the first internal combustion, petroleum-fueled motorcycle. In 1894, Hildebrand & Wolfmüller became the first series production motorcycle..</p>
                                 </div>
                         </div>
                     </div>
@@ -34,8 +34,8 @@ const BikerClubPage = () => {
                     <div className="col">
                         <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                    <h5 className="card-title">Bulding Your Brand</h5>
+                                    <p className="card-text">There are three major types of motorcycle: street, off-road, and dual purpose. Within these types, there are many sub-types of motorcycles for different purposes. There is often a racing counterpart to each type, such as road racing and street bikes, or motocross including dirt bikes..</p>
                                 </div>
                         </div>
                     </div>
